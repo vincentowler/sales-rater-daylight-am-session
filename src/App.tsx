2ec3d0ed869d9beebbd8ab6bd172f008187b44ef
vincentowler <vincentowler@outlook.com>
2022-11-15 07:04:43 -0800

    day one complete

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <label>Origin Zone</label>
+        <br />
+        <input type="text" />
+      </div>
+      <label>Destination Zone</label>
+      <br />
+      <input type="text" />
+    </>
+  );
+}
+
+export default App;
