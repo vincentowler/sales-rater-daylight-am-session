1b3cc120282cd86b9cf51d6afbcffa008a7c4a6a
vincentowler <vincentowler@outlook.com>
2022-11-16 09:56:33 -0800

    add test

---
+++ tests/app.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("should display city and state when a valid Origin zip is entered", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:5173");
+  await page.getByLabel("Origin Zone").fill("10001");
+  await page.getByLabel("Origin Zone").evaluate((e) => e.blur());
+});
