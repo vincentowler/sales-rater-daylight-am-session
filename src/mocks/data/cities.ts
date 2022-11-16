50b007255b9eed4bc6929c762c0f62389d1eaa10
vincentowler <vincentowler@outlook.com>
2022-11-16 07:40:01 -0800

    Add mock data

---
+++ src/mocks/data/cities.ts
@@ -0,0 +1,18 @@
+type CityStateZip = {
+  city: string;
+  state: string;
+  zips: number[];
+};
+
+export const cityStateZips: CityStateZip[] = [
+  {
+    city: "New York",
+    state: "NY",
+    zips: [10001, 10002],
+  },
+  {
+    city: "Los Angeles",
+    state: "CA",
+    zips: [90001, 90002],
+  },
+];
