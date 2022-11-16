6f66b9d4ecdec35dcd31f260ba3ca6804c0b86a3
vincentowler <vincentowler@outlook.com>
2022-11-16 10:42:18 -0800

    Call centralized urls

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,25 +1,18 @@
 // src/mocks/handlers.js
 import { rest } from "msw";
+import { urls } from "../utils/urlUtils";
 import { accessorials } from "./data/accessorials";
 import { cityStateZips } from "./data/cities";
 
-const baseUrl = import.meta.env.VITE_SALES_RATER_API_BASE_URL;
-
 export const handlers = [
-  rest.get(
-    `${baseUrl}/internal/v1/self-service/rater/accessorials`,
-    (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(accessorials));
-    }
-  ),
-  rest.get(
-    `${baseUrl}/internal/v1/self-service/rater/city-by-zip/:zip`,
-    (req, res, ctx) => {
-      const { zip } = req.params;
-      if (!Number.isInteger(Number(zip))) return res(ctx.status(400));
-      const matchingCity = cityStateZips[zip as unknown as number];
-      if (!matchingCity) return res(ctx.status(404));
-      return res(ctx.status(200), ctx.json(matchingCity));
-    }
-  ),
+  rest.get(urls.accessorials, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(accessorials));
+  }),
+  rest.get(urls.cityByZip(":zip"), (req, res, ctx) => {
+    const { zip } = req.params;
+    if (!Number.isInteger(Number(zip))) return res(ctx.status(400));
+    const matchingCity = cityStateZips[zip as unknown as number];
+    if (!matchingCity) return res(ctx.status(404));
+    return res(ctx.status(200), ctx.json(matchingCity));
+  }),
 ];
