ec82349f6bfe63c40aa5d932410fc660549096d3
vincentowler <vincentowler@outlook.com>
2022-11-16 07:40:47 -0800

    Add type

--- src/services/accessorialsApi.ts
+++ src/services/accessorialsApi.ts
@@ -1,8 +1,16 @@
-const baseUrl = import.meta.env.VITE_ACCESSORIALS_API_BASE_URL;
+const baseUrl = import.meta.env.VITE_SALES_RATER_API_BASE_URL;
+
+export type Accessorial = {
+  chargeDescription: string;
+  charge: number;
+  unit: string;
+  min: number;
+  max: number;
+};
 
 export async function getAccessorials() {
   const url = `${baseUrl}/internal/v1/self-service/rater/accessorials`;
   const res = await fetch(url);
   if (!res.ok) throw res;
-  return res.json();
+  return res.json() as Promise<Accessorial[]>;
 }
